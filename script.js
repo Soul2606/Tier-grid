e87a28d0a9d9ffa73bf5d0c50f720bf3e6a62e57
Soul2606 <sondulv@gmail.com>
2025-03-16 19:15:52 +0100

    innit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const form_submit_button = document.getElementById('form-submit-button')
+form_submit_button.addEventListener('click', ()=>{
+    const input_image_files = document.getElementById('input-image-files')
+    const input_rows = Number.parseInt(document.getElementById('amount-of-rows').value)
+    const input_columns = Number.parseInt(document.getElementById('amount-of-columns').value)
+})
