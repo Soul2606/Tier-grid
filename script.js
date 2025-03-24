69b5e8c12d761b2d222c7859a5cbedd3a79cb73e
Soul2606 <sondulv@gmail.com>
2025-03-24 17:22:10 +0100

    added add column function

--- script.js
+++ script.js
@@ -90,35 +90,95 @@ const color_preset_columns = ['rgb(255, 127, 251)','rgb(225, 127, 255)','rgb(180
 const main_grid = document.getElementById('main-grid')
 const add_column_button = document.getElementById('add-column-button')
 const add_row_button = document.getElementById('add-row-button')
+const columns_description_container = document.getElementById('columns-description-container')
+const row_description_container = document.getElementById('row-description-container')
 
 
 
-function add_row(row, columns, start_column) {
-	if (typeof columns === 'number' && typeof row === 'number' && typeof start_column === 'number') {
+
+
+function add_row(start_row, columns_amount, start_column, color, text_content) {
+	if (typeof columns_amount === 'number' && typeof start_row === 'number' && typeof start_column === 'number') {
 		const grid_tier = document.createElement('div')
 		grid_tier.className = 'grid-slot grid-tier'
-		grid_tier.style.gridRow = `${row}/${row+1}`
+		grid_tier.style.gridRow = `${start_row}/${start_row+1}`
 		grid_tier.style.gridColumn = `${start_column}/${start_column+1}`
+		grid_tier.style.backgroundColor = color
+		grid_tier.innerHTML = `<p class="tier-name-text">${text_content}</p>`
+
 		main_grid.appendChild(grid_tier)
-		for (let i = 0; i < columns; i++) {
+		for (let i = 0; i < columns_amount; i++) {
 			const grid_slot = document.createElement('div')
 			grid_slot.className = 'grid-slot drag-destination'
-			grid_slot.style.gridRow = `${row}/${row+1}`
+			grid_slot.style.gridRow = `${start_row}/${start_row+1}`
 			grid_slot.style.gridColumn = `${i+start_column+1}/${i+start_column+2}`
 			main_grid.appendChild(grid_slot)
 		}
+
 		const lane_options = document.createElement('div')
-		lane_options.className = 'grid-slot lane-options'
-		lane_options.style.gridRow = `${row}/${row+1}`
-		lane_options.style.gridColumn = `${columns+start_column+1}/${columns+start_column+2}`
+		lane_options.className = 'grid-slot row-options'
+		lane_options.style.gridRow = `${start_row}/${start_row+1}`
+		lane_options.style.gridColumn = `${columns_amount+start_column+1}/${columns_amount+start_column+2}`
 		main_grid.appendChild(lane_options)
 
-		add_row_button.style.gridRow = `${row+1}/${row+2}`
+		add_row_button.style.gridRow = `${start_row+1}/${start_row+2}`
+		row_description_container.style.gridRowEnd = start_row+1
+
+		const column_options_elements = document.getElementsByClassName('column-options')
+		for (const element of column_options_elements) {
+			element.style.gridRow `${start_row+1}/${start_row+2}`
+		}
 	}
 }
 
 
-add_row(8,5,2)
+
+
+function add_column(start_column, rows_amount, start_row, color, text_content) {
+	if (typeof rows_amount === 'number' && typeof start_row === 'number' && typeof start_column === 'number') {
+		const grid_tier = document.createElement('div')
+		grid_tier.className = 'grid-slot grid-tier'
+		grid_tier.style.gridColumn = `${start_column}/${start_column+1}`
+		grid_tier.style.gridRow = `${start_row}/${start_row+1}`
+		grid_tier.style.backgroundColor = color
+		grid_tier.innerHTML = `<p class="tier-name-text">${text_content}</p>`
+
+		main_grid.appendChild(grid_tier)
+		for (let i = 0; i < rows_amount; i++) {
+			const grid_slot = document.createElement('div')
+			grid_slot.className = 'grid-slot drag-destination'
+			grid_slot.style.gridColumn = `${start_column}/${start_column+1}`
+			grid_slot.style.gridRow = `${i+start_row+1}/${i+start_row+2}`
+			main_grid.appendChild(grid_slot)
+		}
+
+		const lane_options = document.createElement('div')
+		lane_options.className = 'grid-slot column-options'
+		lane_options.style.gridColumn = `${start_column}/${start_column+1}`
+		lane_options.style.gridRow = `${rows_amount+start_row+1}/${rows_amount+start_row+2}`
+		main_grid.appendChild(lane_options)
+
+		add_column_button.style.gridColumn = `${start_column+1}/${start_column+2}`
+		columns_description_container.style.gridColumnEnd = start_column+1
+
+		const row_options_elements = document.getElementsByClassName('row-options')
+		for (const element of row_options_elements) {
+			element.style.gridColumn = `${start_column+1}/${start_column+2}`
+		}
+	}
+}
+
+
+
+
+add_row(3,0,2, color_preset_rows[0], 'S')
+add_row(4,0,2, color_preset_rows[1], 'A')
+add_row(5,0,2, color_preset_rows[2], 'B')
+add_row(6,0,2, color_preset_rows[3], 'C')
+add_column(3,4,2, color_preset_columns[0], '&omega;')
+add_column(4,4,2, color_preset_columns[1], '&alpha;')
+add_column(5,4,2, color_preset_columns[2], '&beta;')
+add_column(6,4,2, color_preset_columns[3], '&gamma;')
 
 
 
